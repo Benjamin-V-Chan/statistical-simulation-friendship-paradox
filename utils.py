2784bc8110cd30a50da3c3f4c6983061dc94ee04
Benjamin Chan <benjamin.v.chan@gmail.com>
2025-01-23 21:50:25 -0800

    utils file complete

---
+++ utils.py
@@ -0,0 +1,5 @@
+# Utility functions for:
+# - Generating random graphs (e.g., Erdos-Renyi, Barabasi-Albert)
+# - Computing degree and average neighbor degree
+# - Saving and loading data
+# - Creating plots
